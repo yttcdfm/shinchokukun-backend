26c261d90b18f0b0eccbd871dbd8bf373c91b781
yttcdfm <ytt.akbakb84@gmail.com>
2024-12-15 13:16:02 +0900

    初回コミット

---
+++ src/main/java/shinchokukun_backend/ShinchokukunBackendApplication.java
@@ -0,0 +1,13 @@
+package shinchokukun_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinchokukunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinchokukunBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shinchokukun_backend/ShinchokukunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package shinchokukun_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinchokukunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
